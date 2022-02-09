ba631f3e0fe5339fa874d4325faf8bf6c98a46b9
Ylarod <me@ylarod.cn>
2022-02-09 19:12:57 +0800

    Update enable_miui_and.sh

--- enable_miui_and.sh
+++ enable_miui_and.sh
@@ -17,25 +17,23 @@ if [ $LINE_NO > 0 ]
 then
     echo "Find security_adb_install_enable at line $LINE_NO"
     sed -i '/security_adb_install_enable/s/false/true/' $XML_FILE
-    grep "security_adb_install_enable" $XML_FILE
 else
     echo "Cannot find security_adb_install_enable, inserting..."
     sed -i '3a \ \ \ \ <boolean name="security_adb_install_enable" value="true" />' $XML_FILE
-    grep "security_adb_install_enable" $XML_FILE
 fi
+grep "security_adb_install_enable" $XML_FILE
 
 echo "4. Disable install intercept"
 LINE_NO=`grep -n "permcenter_install_intercept_enabled" $XML_FILE | awk -F: '{print $1}'`
 if [ $LINE_NO > 0 ]
 then
     echo "Find permcenter_install_intercept_enabled at line $LINE_NO"
-    sed -i '/permcenter_install_intercept_enabled/s/false/true/' $XML_FILE
-    grep "permcenter_install_intercept_enabled" $XML_FILE
+    sed -i '/permcenter_install_intercept_enabled/s/true/false/' $XML_FILE
 else
     echo "Cannot find permcenter_install_intercept_enabled, inserting..."
     sed -i '3a \ \ \ \ <boolean name="permcenter_install_intercept_enabled" value="false" />' $XML_FILE
-    grep "permcenter_install_intercept_enabled" $XML_FILE
 fi
+grep "permcenter_install_intercept_enabled" $XML_FILE
 
 kill -9 $(pidof com.miui.securitycenter.remote)
 
